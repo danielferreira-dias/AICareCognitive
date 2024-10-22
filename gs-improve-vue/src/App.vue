1f98e8c72647ac58281da37a97542f67a9884034
danielferreira-dias <142907197+danielferreira-dias@users.noreply.github.com>
2024-10-22 22:10:22 +0100

    Added Navbar Layout

--- gs-improve-vue/src/App.vue
+++ gs-improve-vue/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div
-    class="justify-center flex items-center h-screen bg-gradient-to-t from-zinc-900 via-zinc-800 to-zinc-700">
+    class="flex flex-col md:flex-row items-center h-screen bg-gradient-to-t from-zinc-900 via-zinc-800 to-zinc-700">
+    <Navbar/>
     <Question/>
   </div>
 </template>
 
 <script>
 import Question from './component/Question.vue';
+import Navbar from './component/Navbar.vue';
 export default {
   components:{
-    Question
+    Question,
+    Navbar
   },
   data() {
     return
